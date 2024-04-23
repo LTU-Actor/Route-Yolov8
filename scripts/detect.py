97a913f9d134ccfaf33ea107de2434ebdda9eb14
Justin Dombecki <jdombecki@ltu.edu>
2024-04-23 16:22:28 -0400

    Bug Fixes

--- scripts/detect.py
+++ scripts/detect.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 ########################################################################
-import rospy  # ROS API
-from sensor_msgs.msg import Image  # Image message
-from std_msgs.msg import UInt8, UInt32, String  # UInt8(used as bool) and UInt32(used as +ve number) message
-import ultralytics  # Yolo v8
-import numpy as np  # numpy
-import torch
 import gc  # Garbage collection
 import time
+
 import cv2  # OpenCV version 2.x (ROS limitation)
+import numpy as np  # numpy
+import rospy  # ROS API
+import torch
+import ultralytics  # Yolo v8
 from cv_bridge import CvBridge, CvBridgeError  # ROS to/from OpenCV API
 from dynamic_reconfigure.server import Server  # ROS Parameter server for debug
 from ltu_actor_route_yolov8_detector.cfg import YoloDetectConfig  # packageName.cfg
+from sensor_msgs.msg import Image  # Image message
+from std_msgs.msg import (  # UInt8(used as bool) and UInt32(used as +ve number) message
+    String,
+    UInt8,
+    UInt32,
+)
 
 ########################################################################
 ### Global Variables:
@@ -23,9 +28,9 @@ bridge = CvBridge()
 global model_stop_path  # Get yolov8 stop sign detection model's path
 global model_coco_path  # Get yolov8 COCO trained model's path
 global model_tire_path  # Get yolov8 tire detection model's path
-global model_u_model_path # Get yolov8 u_model path
+global model_u_model_path  # Get yolov8 u_model path
 
-global person_box # Bounding box for person object
+global person_box  # Bounding box for person object
 
 global display_size
 display_size = 640  # pixel resolution used for debug outputs
@@ -41,6 +46,7 @@ yolo_called = False  # True if yolo detection is called for from topic
 
 # This is a comment
 
+
 # Image callback - Converts ROS Image to OpenCV Image
 def get_image_callback(Image):
     # if Yolo is enabled from Dynamic reconfigure
@@ -53,7 +59,9 @@ def get_image_callback(Image):
         # Now cv_image is a standard OpenCV matrice ---------------------------------------
 
         # Resize according to dynamic reconfigure settings
-        cv_image = resize_image(cv_image, size=(config_.image_resize, config_.image_resize))
+        cv_image = resize_image(
+            cv_image, size=(config_.image_resize, config_.image_resize)
+        )
 
         # find the pixel resolution [h x w] from image as received
         height = cv_image.shape[0]
@@ -76,14 +84,29 @@ def yolo_look_for_object_callback(String):
     yolo_called = True
     # When called
     look_for = String.data
-    if look_for in {"stop sign", "stop-sign", "stop", "sign", "signs", "stop signs", "stop-signs", "tire", "tires", 
-                    "pothole", "potholes", "pedestrian", "pedestrians", "person", "persons"}:
+    if look_for in {
+        "stop sign",
+        "stop-sign",
+        "stop",
+        "sign",
+        "signs",
+        "stop signs",
+        "stop-signs",
+        "tire",
+        "tires",
+        "pothole",
+        "potholes",
+        "pedestrian",
+        "pedestrians",
+        "person",
+        "persons",
+    }:
         detect_object()
     # if look_for in {"tire", "tires"}:
     #     detect_tire()
     # if look_for in {"pedestrian", "pedestrians", "person", "persons"}:
     #     detect_person()
-    #^^^ This allows to efficiently run these detections only when needed
+    # ^^^ This allows to efficiently run these detections only when needed
 
     # Once detection is complete, call has ended
     yolo_called = False
@@ -110,7 +133,9 @@ def resize_image(img, size=(display_size, display_size)):
     if h == w:
         return cv2.resize(img, size, cv2.INTER_AREA)
     dif = h if h > w else w
-    interpolation = cv2.INTER_AREA if dif > (size[0] + size[1]) // 2 else cv2.INTER_CUBIC
+    interpolation = (
+        cv2.INTER_AREA if dif > (size[0] + size[1]) // 2 else cv2.INTER_CUBIC
+    )
     x_pos = (dif - w) // 2
     y_pos = (dif - h) // 2
     if len(img.shape) == 2:  # Grayscale images
@@ -170,9 +195,9 @@ def infer_image_using(
 # Take yolo v8 results object and analyze them for classes and bounding boxes
 def analyze_results(results, classes, image_size_in_sq_pixels=409600):
     # Outputs initialization:
-    global person_box
-    detected = np.zeros(4)
-    biggest_bounding_boxes = np.zeros(4)
+    person_box = None
+    detected = []
+    biggest_bounding_boxes = []
     stop_sign_detected = 0
     tire_detected = 0
     pothole_detected = 0
@@ -191,48 +216,72 @@ def analyze_results(results, classes, image_size_in_sq_pixels=409600):
 
         for idx, box in enumerate(boxes):  # Iterate bounding boxes and find the largest
             label = labels[int(box.cls)]  # Get the class label for this box
-            if label == "stop-sign":  # Check if label matches the class we are looking for
+            if (
+                label == "stop-sign"
+            ):  # Check if label matches the class we are looking for
                 stop_sign_detected += 1  # Counter for individual detections
                 # Find the width and height of the bounding box
                 sign_box_width = box.xywh[0][2]
                 sign_box_height = box.xywh[0][3]
-                sign_area = 100 * ((sign_box_width * sign_box_height) / image_size_in_sq_pixels)  # Percent Area
-                if sign_area > stop_sign_biggest_bounding_box:  # Store the largest bounding box
+                sign_area = 100 * (
+                    (sign_box_width * sign_box_height) / image_size_in_sq_pixels
+                )  # Percent Area
+                if (
+                    sign_area > stop_sign_biggest_bounding_box
+                ):  # Store the largest bounding box
                     stop_sign_biggest_bounding_box = sign_area
             if label == "tire":  # Check if label matches the class we are looking for
                 tire_detected += 1  # Counter for individual detections
                 # Find the width and height of the bounding box
                 tire_box_width = box.xywh[0][2]
                 tire_box_height = box.xywh[0][3]
-                tire_area = 100 * ((tire_box_width * tire_box_height) / image_size_in_sq_pixels)  # Percent Area
-                if tire_area > tire_biggest_bounding_box:  # Store the largest bounding box
+                tire_area = 100 * (
+                    (tire_box_width * tire_box_height) / image_size_in_sq_pixels
+                )  # Percent Area
+                if (
+                    tire_area > tire_biggest_bounding_box
+                ):  # Store the largest bounding box
                     tire_biggest_bounding_box = tire_area
-            if label == "pothole":  # Check if label matches the class we are looking for
+            if (
+                label == "pothole"
+            ):  # Check if label matches the class we are looking for
                 pothole_detected += 1  # Counter for individual detections
                 # Find the width and height of the bounding box
                 pothole_box_width = box.xywh[0][2]
                 pothole_box_height = box.xywh[0][3]
-                pothole_area = 100 * ((pothole_box_width * pothole_box_height) / image_size_in_sq_pixels)  # Percent Area
-                if pothole_area > pothole_biggest_bounding_box:  # Store the largest bounding box
+                pothole_area = 100 * (
+                    (pothole_box_width * pothole_box_height) / image_size_in_sq_pixels
+                )  # Percent Area
+                if (
+                    pothole_area > pothole_biggest_bounding_box
+                ):  # Store the largest bounding box
                     pothole_biggest_bounding_box = pothole_area
             if label == "person":  # Check if label matches the class we are looking for
                 person_detected += 1  # Counter for individual detections
                 # Find the width and height of the bounding box
                 person_box_width = box.xywh[0][2]
                 person_box_height = box.xywh[0][3]
-                person_area = 100 * ((person_box_width * person_box_height) / image_size_in_sq_pixels)  # Percent Area
-                if person_area > person_biggest_bounding_box:  # Store the largest bounding box
+                person_area = 100 * (
+                    (person_box_width * person_box_height) / image_size_in_sq_pixels
+                )  # Percent Area
+                if (
+                    person_area > person_biggest_bounding_box
+                ):  # Store the largest bounding box
                     person_biggest_bounding_box = person_area
-                    person_box = box # Store person boudning box for vest detection
+                    person_box = box  # Store person boudning box for vest detection
 
     # Store detected object info and bounding box area in arrays
-    detected[0, 1, 2, 3] = [stop_sign_detected, tire_detected, pothole_detected, person_detected]
-    biggest_bounding_boxes[0, 1, 2, 3] = [stop_sign_biggest_bounding_box, tire_biggest_bounding_box, 
-                                                  pothole_biggest_bounding_box, person_biggest_bounding_box]
-
+    detected.append(stop_sign_detected)
+    detected.append(tire_detected)
+    detected.append(pothole_detected)
+    detected.append(person_detected)
+    biggest_bounding_boxes.append(int(stop_sign_biggest_bounding_box * 100))
+    biggest_bounding_boxes.append(int(tire_biggest_bounding_box * 100))
+    biggest_bounding_boxes.append(int(pothole_biggest_bounding_box * 100))
+    biggest_bounding_boxes.append(int(person_biggest_bounding_box * 100))
 
     # Return the count of detections, max bounding box size and plotted image
-    return (detected, int(biggest_bounding_boxes * 100), person_box, results_image)
+    return (detected, biggest_bounding_boxes, person_box, results_image)
 
 
 # ----------------------------------------------------------------
@@ -256,13 +305,16 @@ def clear_gpu_memory():
 
 # Runs unified model object detection
 
+
 def detect_object():
     # Detect objects
-    (objects_detected, objects_biggest_bounding_boxes, person_box, results_image) = analyze_results(
-        infer_image_using(_path=model_u_model_path, _source=cam_image),
-        classes={"stop-sign", "tire", "pothole", "person"},
-        image_size_in_sq_pixels=(cam_image.shape[0] * cam_image.shape[1]),
-    )  
+    (objects_detected, objects_biggest_bounding_boxes, person_box, results_image) = (
+        analyze_results(
+            infer_image_using(_path=model_u_model_path, _source=cam_image),
+            classes={"stop-sign", "tire", "pothole", "person"},
+            image_size_in_sq_pixels=(cam_image.shape[0] * cam_image.shape[1]),
+        )
+    )
 
     stop_sign_msg = UInt8()
     stop_sign_size_msg = UInt32()
@@ -286,19 +338,19 @@ def detect_object():
     # index 3 = person biggest box
 
     # Publish stop sign data
-    if objects_detected[0] > 0: # stop signs detected > 0
+    if objects_detected[0] > 0:  # stop signs detected > 0
         stop_sign_msg.data = objects_detected[0]
         sign_detect_pub.publish(stop_sign_msg)
         stop_sign_size_msg.data = objects_biggest_bounding_boxes[0]
         sign_size_pub.publish(stop_sign_size_msg)
-    else:  
+    else:
         stop_sign_msg.data = 0
         sign_detect_pub.publish(stop_sign_msg)
         stop_sign_size_msg.data = 0
         sign_size_pub.publish(stop_sign_size_msg)
 
     # Publish tire data
-    if objects_detected[1] > 0: # tires detected > 0
+    if objects_detected[1] > 0:  # tires detected > 0
         tire_msg.data = objects_detected[1]
         tire_detect_pub.publish(tire_msg)
         tire_size_msg.data = objects_biggest_bounding_boxes[1]
@@ -310,7 +362,7 @@ def detect_object():
         tire_size_pub.publish(tire_size_msg)
 
     # Publish pothole data
-    if objects_detected[2] > 0: # potholes detected > 0
+    if objects_detected[2] > 0:  # potholes detected > 0
         pothole_msg.data = objects_detected[2]
         pothole_detect_pub.publish(pothole_msg)
         pothole_size_msg.data = objects_biggest_bounding_boxes[2]
@@ -322,7 +374,7 @@ def detect_object():
         pothole_size_pub.publish(pothole_size_msg)
 
     # Publish person data
-    if objects_detected[3] > 0: # persons detected > 0
+    if objects_detected[3] > 0:  # persons detected > 0
         person_msg.data = objects_detected[3]
         person_detect_pub.publish(person_msg)
         person_size_msg.data = objects_biggest_bounding_boxes[3]
@@ -332,13 +384,13 @@ def detect_object():
         person_detect_pub.publish(person_msg)
         person_size_msg.data = 0
         person_size_pub.publish(person_size_msg)
-    
+
     if config_.debug:
         # Show the detected images real-time for debug
         debug_img = bridge.cv2_to_imgmsg(results_image, "bgr8")
         object_debug_pub.publish(debug_img)
 
-    if config_.enable_vest_mask: # Show the vest mask
+    if config_.enable_vest_mask and person_box is not None:  # Show the vest mask
         # Get the results image, make it black and paste the person bounding box with mask applied onto image
         hsv_image = cv2.cvtColor(results_image, cv2.COLOR_BGR2HSV)
 
@@ -355,19 +407,23 @@ def detect_object():
         person_box_height = person_box.xywh[0][3]
 
         # Extract the bounding box from the mask image
-        vest_mask_img_box = mask[person_box_y_pos:(person_box_y_pos + person_box_height),
-                             person_box_x_pos:(person_box_x_pos + person_box_width)]
-        
+        vest_mask_img_box = mask[
+            person_box_y_pos : (person_box_y_pos + person_box_height),
+            person_box_x_pos : (person_box_x_pos + person_box_width),
+        ]
+
         # Apply the black_mask to the mask image to make black_img
         black_mask = np.zeros_like(mask)
-        black_mask[:,:] = (0, 0, 0)
+        black_mask[:, :] = (0, 0, 0)
         black_img = cv2.bitwise_and(mask, black_mask)
 
         # Paste the vest_mask_img_box onto the black_img
-        black_img[person_box_y_pos:(person_box_y_pos + person_box_height), 
-                  person_box_x_pos:(person_box_x_pos + person_box_width)] = vest_mask_img_box
+        black_img[
+            person_box_y_pos : (person_box_y_pos + person_box_height),
+            person_box_x_pos : (person_box_x_pos + person_box_width),
+        ] = vest_mask_img_box
 
-        #nConvert new black image to imgmsg
+        # nConvert new black image to imgmsg
         debug_img = bridge.cv2_to_imgmsg(black_img, "bgr8")
         vest_mask_pub.publish(debug_img)
 
@@ -482,13 +538,17 @@ if __name__ == "__main__":
     rospy.init_node("route_yolov8_detector", anonymous=False)
 
     # Dynamic Reconfigure parameter server
-    srv = Server(YoloDetectConfig, dyn_rcfg_cb)  # Using common cfg file for entire project
+    srv = Server(
+        YoloDetectConfig, dyn_rcfg_cb
+    )  # Using common cfg file for entire project
 
     # Load the YOLO models at startup
     model_coco_path = rospy.get_param("~model_coco_path_from_root")  # Load latest path
     model_stop_path = rospy.get_param("~model_stop_path_from_root")  # Load latest path
     model_tire_path = rospy.get_param("~model_tire_path_from_root")  # Load latest path
-    model_u_model_path = rospy.get_param("~model_u_model_path_from_root") # Load u_model path
+    model_u_model_path = rospy.get_param(
+        "~model_u_model_path_from_root"
+    )  # Load u_model path
 
     # Image input from topic - from launch file
     imgtopic = rospy.get_param("~imgtopic_name")
@@ -503,41 +563,61 @@ if __name__ == "__main__":
     sign_detect_topic = rospy.get_param("~stop_sign_detected_topic_name")
     sign_size_topic = rospy.get_param("~stop_sign_size_topic_name")
     # sign_debug_topic = rospy.get_param("~stop_sign_debug_topic_name")
-    sign_detect_pub = rospy.Publisher(sign_detect_topic, UInt8, queue_size=1)  # No. of signs detected
-    sign_size_pub = rospy.Publisher(sign_size_topic, UInt32, queue_size=1)  # Biggest sign as % area of image
+    sign_detect_pub = rospy.Publisher(
+        sign_detect_topic, UInt8, queue_size=1
+    )  # No. of signs detected
+    sign_size_pub = rospy.Publisher(
+        sign_size_topic, UInt32, queue_size=1
+    )  # Biggest sign as % area of image
     # sign_debug_pub = rospy.Publisher(sign_debug_topic, Image, queue_size=1)  # Anotated image debug output
 
     # person detection:
     person_detect_topic = rospy.get_param("~person_detected_topic_name")
     person_size_topic = rospy.get_param("~person_size_topic_name")
     # person_debug_topic = rospy.get_param("~person_debug_topic_name")
-    person_detect_pub = rospy.Publisher(person_detect_topic, UInt8, queue_size=1)  # No. of persons detected
-    person_size_pub = rospy.Publisher(person_size_topic, UInt32, queue_size=1)  # Biggest person as % area of image
+    person_detect_pub = rospy.Publisher(
+        person_detect_topic, UInt8, queue_size=1
+    )  # No. of persons detected
+    person_size_pub = rospy.Publisher(
+        person_size_topic, UInt32, queue_size=1
+    )  # Biggest person as % area of image
     # person_debug_pub = rospy.Publisher(person_debug_topic, Image, queue_size=1)  # Anotated image debug output
 
     # Tire detection:
     tire_detect_topic = rospy.get_param("~tire_detected_topic_name")
     tire_size_topic = rospy.get_param("~tire_size_topic_name")
     # tire_debug_topic = rospy.get_param("~tire_debug_topic_name")
-    tire_detect_pub = rospy.Publisher(tire_detect_topic, UInt8, queue_size=1)  # No. of tires detected
-    tire_size_pub = rospy.Publisher(tire_size_topic, UInt32, queue_size=1)  # Biggest tire as % area of image
+    tire_detect_pub = rospy.Publisher(
+        tire_detect_topic, UInt8, queue_size=1
+    )  # No. of tires detected
+    tire_size_pub = rospy.Publisher(
+        tire_size_topic, UInt32, queue_size=1
+    )  # Biggest tire as % area of image
     # tire_debug_pub = rospy.Publisher(tire_debug_topic, Image, queue_size=1)  # Anotated image debug output
 
     # Pothole detection:
     pothole_detect_topic = rospy.get_param("~pothole_detected_topic_name")
     pothole_size_topic = rospy.get_param("~pothole_size_topic_name")
     # pothole_debug_topic = rospy.get_param("~pothole_debug_topic_name")
-    pothole_detect_pub = rospy.Publisher(pothole_detect_topic, UInt8, queue_size=1)  # No. of tires detected
-    pothole_size_pub = rospy.Publisher(pothole_size_topic, UInt32, queue_size=1)  # Biggest tire as % area of image
+    pothole_detect_pub = rospy.Publisher(
+        pothole_detect_topic, UInt8, queue_size=1
+    )  # No. of tires detected
+    pothole_size_pub = rospy.Publisher(
+        pothole_size_topic, UInt32, queue_size=1
+    )  # Biggest tire as % area of image
     # pothole_debug_pub = rospy.Publisher(pothole_debug_topic, Image, queue_size=1)  # Anotated image debug output
 
     # Object debug:
     object_debug_topic = rospy.get_param("~object_debug_topic_name")
-    object_debug_pub = rospy.Publisher(object_debug_topic, Image, queue_size=1) # Anotated image debug output
+    object_debug_pub = rospy.Publisher(
+        object_debug_topic, Image, queue_size=1
+    )  # Anotated image debug output
 
     # >>> Topics and publishers for person vest mask
     vest_mask_topic = rospy.get_param("~vest_mask_topic_name")
-    vest_mask_pub = rospy.Publisher(vest_mask_topic, Image, queue_size=1) # Person bounding box with mask for vest extraction
+    vest_mask_pub = rospy.Publisher(
+        vest_mask_topic, Image, queue_size=1
+    )  # Person bounding box with mask for vest extraction
 
     rospy.spin()  # Runs callbacks
 
